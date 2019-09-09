4654d5a3fbf9d31642ef85d1d10969cd93eadbd2
Sam Knowles <samuelkcknowles@gmail.com>
2019-09-09 11:18:17 -0600

    finished product

--- js/app.js
+++ js/app.js
@@ -5,5 +5,33 @@ const unContainer = document.querySelector(".main__profile-username");
 const reposContainer = document.querySelector(".main__profile-repos");
 const urlContainer = document.querySelector(".main__profile-url");
 
-const client_id = "";
-const client_secret = "";
+const client_id = "3tzPytTYEEwct2e";
+const client_secret = "ddcb645aee669564615269b63f9ef415dc504fa4";
+
+const fetchUsers = async (user) => {
+    const api_call = await fetch(`https://api.github.com/users/${user}?client_id=${client_id}&client_secret=${client_secret}`);
+    
+    const data = await api_call.json();
+    return { data: data }
+};
+
+const showData = () => {
+    fetchUsers(inputValue.value).then((res) => {
+
+        nameContainer.innerHTML = `Name: <span class='main__profile-value'>${res.data.name}</span`;
+
+        unContainer.innerHTML = `Name: <span class='main__profile-value'>${res.data.login}</span`;
+
+        reposContainer.innerHTML = `Name: <span class='main__profile-value'>${res.data.public_repos}</span`;
+
+        urlContainer.innerHTML = `Name: <span class='main__profile-value'>${res.data.html_url}</span`;
+
+    })
+}
+
+
+
+
+searchButton.addEventListener('click', () => {
+    showData();
+})
